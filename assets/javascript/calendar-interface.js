06c44af16d9b93e0518a4dd9fe97f96a2ee45d08
psgs <fpigerre@gmail.com>
2015-06-02 17:32:50 +1000

    Begin implementing metro screen
    Begin implementing event metro layout, refactor JavaScript utilities to
    a new file.

--- assets/javascript/calendar-interface.js
+++ assets/javascript/calendar-interface.js
@@ -80,10 +80,13 @@ function loadCalendarData() {
 
         if (events.length > 0) {
             var container = document.getElementsByClassName('upcoming');
+            var node = '';
 
             for (var i = 0; i < container.length; i++) {
                 var event = events[i];
                 var when = event.start.dateTime;
+                node = node + '<div class=\"metro-box\">';
+                console.log(node);
 
                 if (!when) {
                     // Use the event's date
@@ -99,17 +102,21 @@ function loadCalendarData() {
                 }
 
                 if (event.description && event.description.includes(":icon:")) {
-                    var icon = event.description.split(":icon:").pop();
+                    var icon = '<i class=\"' +  event.description.split(":icon:").pop() + '\"></i>'
                 }
 
                 // Append event data to a container object
                 if (icon) {
-                    container[i].innerHTML = '<h3>' + when + '</h3><i class=\"' + icon + '\"></i><p>' + event.summary + '</p>';
+                    container[i].innerHTML = '<h3>' + when + '</h3>' + icon + '<p>' + event.summary + '</p>';
+                    node = node + icon;
                     icon = null;
                 } else {
                     container[i].innerHTML = '<h3>' + when + '</h3><p>' + event.summary + '</p>';
                 }
+
+                node = node + '</div>';
             }
+            document.getElementById("metro").innerHTML = node;
         } else {
             // TODO: Handle lack of events
         }
@@ -124,91 +131,4 @@ function initializeProfileData() {
     profileRequest.execute(function (response) {
         updateName(response.displayName);
     });
-}
-
-/**
- * Appends a suffix to a number, for example, the number 3
- * returns "3rd".
- *
- * @param i A number
- * @returns {string} The number with a suffix attached
- */
-function appendSuffix(i) {
-    i = parseInt(i, 10);
-
-    var j = i % 10,
-        k = i % 100;
-    if (j == 1 && k != 11) {
-        return i + "st";
-    }
-    if (j == 2 && k != 12) {
-        return i + "nd";
-    }
-    if (j == 3 && k != 13) {
-        return i + "rd";
-    }
-    return i + "th";
-}
-
-/**
- * Gets the name of a month from an integer
- *
- * @param i An integer between 1 and 12
- * @returns {string} The name of a month, formatted as a string
- */
-function getMonthString(i) {
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-    i = parseInt(i, 10);
-    return monthNames[i];
-}
-
-/**
- * Gets the name of a day from an integer
- *
- * @param i An integer between 1 and 7
- * @returns {string} The name of a day, formatted as a string
- */
-function getDayString(i) {
-    var dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    i = parseInt(i, 10);
-    return dayNames[i];
-}
-
-/**
- * Update the main page with a friendly greeting featuring the
- * user's first name.
- *
- * @param fullName {string} The user's full name
- */
-function updateName(fullName) {
-    var greetings = ["Hi", "Hello", "Hey", "Howdy"];
-    var firstName = fullName.split(' ')[0];
-    document.getElementById('name').innerHTML
-        = greetings[Math.floor(Math.random() * greetings.length)]
-        + " "
-        + firstName
-        + "!";
-}
-
-/**
- * Launch a real-time clock
- */
-function launchClock() {
-    var time;
-    var date = new Date(Date.now());
-
-    document.getElementById("date").innerHTML
-        = getDayString(date.getDay())
-        + ' '
-        + appendSuffix(date.getDate())
-        + ' of '
-        + getMonthString(date.getMonth());
-
-    setInterval(function () {
-        time = new Date(Date.now());
-        document.getElementById("time").innerHTML = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
-        time = null;
-    }, 900);
 }
---
+++ assets/javascript/display-utilities.js
@@ -0,0 +1,86 @@
+/**
+ * Appends a suffix to a number, for example, the number 3
+ * returns "3rd".
+ *
+ * @param i A number
+ * @returns {string} The number with a suffix attached
+ */
+function appendSuffix(i) {
+    i = parseInt(i, 10);
+
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+
+/**
+ * Gets the name of a month from an integer
+ *
+ * @param i An integer between 1 and 12
+ * @returns {string} The name of a month, formatted as a string
+ */
+function getMonthString(i) {
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    i = parseInt(i, 10);
+    return monthNames[i];
+}
+
+/**
+ * Gets the name of a day from an integer
+ *
+ * @param i An integer between 1 and 7
+ * @returns {string} The name of a day, formatted as a string
+ */
+function getDayString(i) {
+    var dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    i = parseInt(i, 10);
+    return dayNames[i];
+}
+
+/**
+ * Update the main page with a friendly greeting featuring the
+ * user's first name.
+ *
+ * @param fullName {string} The user's full name
+ */
+function updateName(fullName) {
+    var greetings = ["Hi", "Hello", "Hey", "Howdy"];
+    var firstName = fullName.split(' ')[0];
+    document.getElementById('name').innerHTML
+        = greetings[Math.floor(Math.random() * greetings.length)]
+        + " "
+        + firstName
+        + "!";
+}
+
+/**
+ * Launch a real-time clock
+ */
+function launchClock() {
+    var time;
+    var date = new Date(Date.now());
+
+    document.getElementById("date").innerHTML
+        = getDayString(date.getDay())
+        + ' '
+        + appendSuffix(date.getDate())
+        + ' of '
+        + getMonthString(date.getMonth());
+
+    setInterval(function () {
+        time = new Date(Date.now());
+        document.getElementById("time").innerHTML = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
+        time = null;
+    }, 900);
+}
